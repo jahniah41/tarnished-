4fdf726e151111235c9f0d2ba32ec0bc4401980e
jahniah41 <jahniah60@gmail.com>
2023-12-06 15:37:27 -0500

    commoit

---
+++ website1.js
@@ -0,0 +1,15 @@
+function player () {
+const player1 = {
+firstName: `jahniah`
+,lastName: `sanders`
+,level: 1
+,class: `mage`
+}
+const player2 = {
+    firstName: `john`
+    ,lastName:`smith`
+    ,level: 2
+    ,class:`warrior`
+}
+
+}
