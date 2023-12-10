cd3ba02b689d652860bc8d1194e01b876d3e337c
jahniah41 <148720087+jahniah41@users.noreply.github.com>
2023-12-10 18:17:59 +0000

    added player1 and player2 dot notations to origin

--- website1.js
+++ website1.js
@@ -5,11 +5,18 @@ firstName: `jahniah`
 ,level: 1
 ,class: `mage`
 }
+player1.firstName;
+player1.lastName;
+player1.level;
+player1.class;
 const player2 = {
     firstName: `john`
     ,lastName:`smith`
     ,level: 2
     ,class:`warrior`
 }
-
+player2.firstName;
+player2.lastName;
+player2.level;
+player2.class;
 }
