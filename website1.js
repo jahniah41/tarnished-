5dce599a9267ed42e85fef80d8f967c2d2dd4627
jahniah41 <148720087+jahniah41@users.noreply.github.com>
2023-12-11 07:13:03 +0000

    added selected stats to code

--- website1.js
+++ website1.js
@@ -9,6 +9,12 @@ player1.firstName;
 player1.lastName;
 player1.level;
 player1.class;
+player1.stats = {}
+stats.sTG = 2
+stats.dEX = 4
+stats.cRM = 3
+stats.iNT = 5
+stats.aRC = 4
 const player2 = {
     firstName: `john`
     ,lastName:`smith`
@@ -19,4 +25,11 @@ player2.firstName;
 player2.lastName;
 player2.level;
 player2.class;
+player2.stats2 = {}
+    stats2.sTG2 = 5
+    stats2.dEX2 = 3
+    stats2.cRM2 = 2
+    stats2.iNT2 = 1
+    stats2.aRC2 = 1
 }
+
